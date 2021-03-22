1b5d2e520d10be0017c0d18a9be121a66fbdb24a
Manh Cuong <cuong@radioactive.sg>
2021-03-22 09:26:16 +0800

    add gitignore

--- fb_message2.py
+++ fb_message2.py
@@ -10,38 +10,45 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import WebDriverException
 
-options = FirefoxOptions()
-#options.add_argument("--headless")
+def check_user(user_name, password, http_proxy, headless):
+    check = True
+    options = FirefoxOptions()
+    if headless:
+        options.add_argument("--headless")
 
-desired_capability = webdriver.DesiredCapabilities.FIREFOX
-desired_capability['proxy']={
-    "proxyType":"manual",
-    "httpProxy": "94.140.228.197:81",
-}        
+    desired_capability = webdriver.DesiredCapabilities.FIREFOX
+    desired_capability['proxy'] = {
+        "proxyType": "manual",
+        "httpProxy": http_proxy,
+    }
 
-firefox_profile = webdriver.FirefoxProfile()
-firefox_profile.set_preference("browser.privatebrowsing.autostart", True)
-driver = webdriver.Firefox(options=options, firefox_profile=firefox_profile, capabilities=desired_capability) 
+    firefox_profile = webdriver.FirefoxProfile()
+    #firefox_profile.set_preference("browser.privatebrowsing.autostart", True)
+    driver = webdriver.Firefox(
+        options=options, firefox_profile=firefox_profile, capabilities=desired_capability)
 
-driver.get("https://facebook.com")
+    driver.get("https://facebook.com")
 
-elem = driver.find_element_by_name("email")
-elem.clear()
-elem.send_keys(user.fb_user)
+    elem = driver.find_element_by_name("email")
+    elem.clear()
+    elem.send_keys(user_name)
 
-elem = driver.find_element_by_name("pass")
-elem.clear()
-elem.send_keys(user.fb_pass)
+    elem = driver.find_element_by_name("pass")
+    elem.clear()
+    elem.send_keys(password)
 
-elem.send_keys(Keys.RETURN)
+    elem.send_keys(Keys.RETURN)
 
-try:
-    WebDriverWait(driver, 10).until(
+    try:
+        WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.XPATH, '//a[@href="/groups/"]'))
-    )
-    print("Headless Firefox Initialized")
-except WebDriverException:
-    print("abcd")
+        )
+    except WebDriverException:
+        check = False 
+        
+    driver.quit()
+    return check
 
+if __name__ == "__main__":
+    print(check_user(user.fb_user, user.fb_pass, "94.140.228.197:81", True))
 
-driver.quit()       
--- user.py
+++ user.py
@@ -1,3 +1,2 @@
-group_id = '646058272449994'
 fb_user = 'vodanhbk172@gmail.com'
 fb_pass = 'cuong$2711'
